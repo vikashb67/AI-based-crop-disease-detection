c1115b7487624d0b494246e68942457a9a4867ed
g-pranav-kaushik <gpranavkaushik06@gmail.com>
2026-05-02 21:40:41 +0530

    program practice

--- model/worpy.py
+++ model/worpy.py
@@ -1,1 +1,14 @@
-print("this is a python file in model to understand working of git and github")
+a=[]
+n=int(input("Enter the number of elements: "))
+for i in range(n):
+    element=int(input("Enter the element: "))
+    a.append(element)
+max=a[0]
+for i in range(1,n):
+    if a[i]>max:
+        max=a[i]
+smax=a[0]
+for i in range(1,n):
+    if a[i]>smax and a[i]!=max:
+        smax=a[i]
+print("The second largest element is: ",smax)
