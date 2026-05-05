776f75cadfcff692ef6030acc95c16e62691987c
g-pranav-kaushik <gpranavkaushik06@gmail.com>
2026-05-05 16:03:57 +0530

    program practice

--- ieee/rev.py
+++ ieee/rev.py
@@ -1,17 +1,2 @@
-import numpy as np
-
-a= int(input("enter the 1st number: "))
-b= int(input("enter the 2nd number: "))
-c=input("enter the operator: ")
-if c=="+":
-    print(a+b)
-elif c=="-":
-    print(a-b)
-elif c=="*":
-    print(a*b)
-elif c=="/":
-    print(a/b)
-elif c=="%":
-    print(a%b)
-else:    
-    print("invalid operator")  
+f= open("/Users/gpranavkaushik/ieee git/ieee/datanew.txt", "r")
+print(f.readline())
